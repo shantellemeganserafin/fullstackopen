f502f24058fec8c8e51ce738dfc20d209254b481
shantellemeganserafin <shantellemegan@gmail.com>
2024-06-27 14:15:08 -0700

    include updated dist

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -9,7 +9,7 @@ const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(false)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService
