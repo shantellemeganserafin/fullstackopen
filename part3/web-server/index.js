79a42f210112887132be2fc77aa1cd4b7639a08d
shantellemeganserafin <shantellemegan@gmail.com>
2024-07-01 15:05:43 -0700

    added minimum note length

--- part3/web-server/index.js
+++ part3/web-server/index.js
@@ -45,12 +45,8 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
   
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-  
-    if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
-    }
   
     const note = new Note({
       content: body.content,
@@ -61,17 +57,17 @@ app.post('/api/notes', (request, response) => {
       console.log('noted saved!')
       response.json(savedNote)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  ) 
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -88,7 +84,10 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
   next(error)
 }
 app.use(errorHandler) //handles errors passed to `next`
--- part3/web-server/models/note.js
+++ part3/web-server/models/note.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URL
+const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
@@ -16,8 +16,12 @@ mongoose.connect(url)
   })
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean
 })
 
 noteSchema.set('toJSON', {
